88adfa8bfcc3c2592cd1e1a4b316cb54e80f9910
Manuel Ramirez <manramloz@gmail.com>
2024-01-09 22:16:40 +0100

    creo esquema del usuario

---
+++ nodepop/models/Usuario.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+});
+
+const Usuario = mongoose.model('Usuario', usuarioSchema);
+
+module.exports = Usuario;
